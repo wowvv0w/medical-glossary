5780957322918f743348af4ae60b39a48ff7c180
wowvv0w <wowvv0w@naver.com>
2022-08-04 17:56:54 +0900

    fix: use App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import WordTable from './components/WordTable';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+        }
+        this.updateResult = this.updateResult.bind(this);
+    }
+    updateResult(text) {
+        this.setState({text});
+    }
+    render() {
+        return (
+            <React.StrictMode>
+                <Header />
+                <SearchBar onChange={this.updateResult}/>
+                <WordTable text={this.state.text}/>
+            </React.StrictMode>
+        );
+    }
 }
 
 export default App;
