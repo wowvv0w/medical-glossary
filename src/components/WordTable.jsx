87848fb6017f48c305ed811a4d26c27c844c0855
wowvv0w <wowvv0w@naver.com>
2022-08-13 20:55:24 +0900

    feat: add glossary json file

--- src/components/WordTable.jsx
+++ src/components/WordTable.jsx
@@ -4,14 +4,6 @@ import Word from './Word';
 class WordTable extends Component {
     constructor(props) {
         super(props);
-        this.list = [
-            {word: "A", meaning: "Football"},
-            {word: "B", meaning: "Baseball"},
-            {word: "C", meaning: "Basketball"},
-            {word: "AA", meaning: "iPod Touch"},
-            {word: "BB", meaning: "iPhone 5"},
-            {word: "CA", meaning: "Nexus 7"}
-          ];
     }
     filter(text) {
         return text ? this.list.filter((item) => item.word.toLowerCase().startsWith(text.toLowerCase())) : this.list;
